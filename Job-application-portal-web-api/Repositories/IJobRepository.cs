9e8b25f5dfc71eaa97994a3e9baae3bc0b77d770
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 17:43:21 +0530

    Update IJobRepository.cs

--- Job-application-portal-web-api/Repositories/IJobRepository.cs
+++ Job-application-portal-web-api/Repositories/IJobRepository.cs
@@ -1,6 +1,8 @@
+using Job_application_portal_web_api.Models;
+
 namespace Job_application_portal_web_api.Repositories;
 
 public interface IJobRepository
 {
-    
+    Task<Job> CreateJob(Job job);
 }
