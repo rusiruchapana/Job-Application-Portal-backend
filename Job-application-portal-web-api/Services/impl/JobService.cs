8e5aa772aaaad218508f54c5784894b5e953064a
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 16:12:22 +0530

    Update JobService.cs

--- Job-application-portal-web-api/Services/impl/JobService.cs
+++ Job-application-portal-web-api/Services/impl/JobService.cs
@@ -1,6 +1,13 @@
+using Job_application_portal_web_api.Repositories;
+
 namespace Job_application_portal_web_api.Services.impl;
 
 public class JobService: IJobService
 {
-    
+    private readonly IJobRepository _jobRepository;
+
+    public JobService(IJobRepository jobRepository)
+    {
+        _jobRepository = jobRepository;
+    }
 }
