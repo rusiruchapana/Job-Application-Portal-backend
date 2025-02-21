bb2232eb930657c400bfe0bf67ba387262a92675
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 16:12:31 +0530

    Update JobRepository.cs

--- Job-application-portal-web-api/Repositories/impl/JobRepository.cs
+++ Job-application-portal-web-api/Repositories/impl/JobRepository.cs
@@ -1,6 +1,12 @@
+using Job_application_portal_web_api.Data;
+
 namespace Job_application_portal_web_api.Repositories.impl;
 
 public class JobRepository: IJobRepository
 {
-    
+    private readonly ApplicationDbContext _context;
+    public JobRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 }
