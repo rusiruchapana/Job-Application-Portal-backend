fc83ecda08d59c4243947131e18d33f8f631ade0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 16:50:56 +0530

    Create AutomapperProfile.cs

---
+++ Job-application-portal-web-api/Map/AutomapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Job_application_portal_web_api.Dtos.Request;
+using Job_application_portal_web_api.Models;
+
+namespace Job_application_portal_web_api.Map;
+
+public class AutomapperProfile: Profile
+{
+    public AutomapperProfile()
+    {
+        CreateMap<JobRequestDto , Job>();
+    }
+}
