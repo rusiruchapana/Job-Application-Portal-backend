6158cb587c0a59f1ab07be9acc77b55c6cd67810
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 16:11:04 +0530

    Update JobController.cs

--- Job-application-portal-web-api/Controllers/JobController.cs
+++ Job-application-portal-web-api/Controllers/JobController.cs
@@ -1,3 +1,4 @@
+using Job_application_portal_web_api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Job_application_portal_web_api.Controllers;
@@ -6,5 +7,14 @@ namespace Job_application_portal_web_api.Controllers;
 [Route("api/[controller]")]
 public class JobController: ControllerBase
 {
+    private readonly IJobService _jobService;
+    public JobController(IJobService jobService)
+    {
+        _jobService = jobService;
+    }
+    
+    
+    
+    
     
 }
