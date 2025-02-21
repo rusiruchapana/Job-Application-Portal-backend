f3b5dace0587dec6d21c548e54026b4c169195df
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 22:10:18 +0530

    Update JobRepository.cs

--- Job-application-portal-web-api/Repositories/impl/JobRepository.cs
+++ Job-application-portal-web-api/Repositories/impl/JobRepository.cs
@@ -1,5 +1,6 @@
 using Job_application_portal_web_api.Data;
 using Job_application_portal_web_api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Job_application_portal_web_api.Repositories.impl;
 
@@ -16,5 +17,10 @@ public class JobRepository: IJobRepository
         await _context.Jobs.AddAsync(job);
         await _context.SaveChangesAsync();
         return job;
+    }
+
+    public async Task<List<Job>> AllJobs()
+    {
+        return await _context.Jobs.ToListAsync();
     }
 }
