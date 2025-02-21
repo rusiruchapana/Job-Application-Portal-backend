e9b16ee9e569b56359c28cf1a8c72dbdb177d18c
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 16:26:48 +0530

    Update JobService.cs

--- Job-application-portal-web-api/Services/impl/JobService.cs
+++ Job-application-portal-web-api/Services/impl/JobService.cs
@@ -1,3 +1,5 @@
+using Job_application_portal_web_api.Dtos.Request;
+using Job_application_portal_web_api.Dtos.Response;
 using Job_application_portal_web_api.Repositories;
 
 namespace Job_application_portal_web_api.Services.impl;
@@ -9,5 +11,11 @@ public class JobService: IJobService
     public JobService(IJobRepository jobRepository)
     {
         _jobRepository = jobRepository;
+    }
+
+    public async Task<JobResponseDto> CreateJob(JobRequestDto jobRequestDto)
+    {
+        
+        return null;
     }
 }
