705ac72fad9202fe5f5c117a613acd2a5a122c18
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 17:43:04 +0530

    Update JobService.cs

--- Job-application-portal-web-api/Services/impl/JobService.cs
+++ Job-application-portal-web-api/Services/impl/JobService.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using Job_application_portal_web_api.Dtos.Request;
 using Job_application_portal_web_api.Dtos.Response;
+using Job_application_portal_web_api.Models;
 using Job_application_portal_web_api.Repositories;
 
 namespace Job_application_portal_web_api.Services.impl;
@@ -7,14 +9,17 @@ namespace Job_application_portal_web_api.Services.impl;
 public class JobService: IJobService
 {
     private readonly IJobRepository _jobRepository;
+    private readonly IMapper _mapper;
 
-    public JobService(IJobRepository jobRepository)
+    public JobService(IJobRepository jobRepository , IMapper mapper)
     {
         _jobRepository = jobRepository;
+        _mapper = mapper;
     }
 
     public async Task<JobResponseDto> CreateJob(JobRequestDto jobRequestDto)
     {
-        return null;
+        Job job = await _jobRepository.CreateJob(_mapper.Map<Job>(jobRequestDto));
+        return _mapper.Map<JobResponseDto>(job);
     }
 }
